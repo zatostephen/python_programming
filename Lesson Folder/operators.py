fa7cb9dec63eec6b4a70a0ea6e65a3b45e65d7e8
zatostephen <88582953+zatostephen@users.noreply.github.com>
2021-08-21 12:50:58 -0800

    Create opertors

---
+++ Lesson Folder/operators.py
@@ -0,0 +1,62 @@
+# create a variable x and assign a value 10 
+x = 10
+
+#create a variable y and assign 25 
+y = 25
+
+print("X plus Y is",x + y)
+
+print("X minus y is" ,x - y)
+
+print("X divided by Y is ", x / y )
+
+print("The value of x**y is: ", x ** y)
+
+#floor division
+a = 12.68
+b = 3
+print("The value of a//b is: ", a//b)
+
+print("The value of x % y is: ",x%y)
+
+print("The value of -x is: ",-x)		
+print(abs(-x))
+
+string_1 = "Python"
+
+string_2 = "Programming"
+
+finalstring = string_1 + string_2
+
+print(finalstring)
+
+print(string_1 * 4)
+
+print(x == 10)
+
+print(x != y)
+
+print(x > y)
+
+print(x < y)	
+
+print(x <= y)
+
+print(x is 10)	
+
+# error
+
+print(a<< x )
+
+print(a>> x )
+
+print(~a)
+
+
+
+
+
+
+
+
+
