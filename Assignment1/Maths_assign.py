12455bfd60c55b38161f87bd65b2bc2dea75f182
zatostephen <88582953+zatostephen@users.noreply.github.com>
2021-08-21 08:57:14 -0800

    Assignment1

--- Assignment1/Maths_assign.py
+++ Assignment1/Maths_assign.py
@@ -3,8 +3,8 @@
 x=float(input("Enter a valua for x: "))
 z=float(input("Enter a value for z:  "))
 #create a variable that calculate the numerator of the equation and the denom.
-numerator=2*x + 5 - 3*z
-denomerator= 4 - 5*z
+numerator=2*x + 5 - (3*z)
+denomerator= 4 - (5*z)
 y=numerator/denomerator
 #print function to display output
 print("Y is equal to",y)
