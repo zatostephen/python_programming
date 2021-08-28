d44a28f7189c855b3ed67bbecf4d6f616dca7ac6
zatostephen <88582953+zatostephen@users.noreply.github.com>
2021-08-28 12:09:12 -0800

    create if_and_else_statement

---
+++ Lesson Folder/If_Statement_OR Boolen.py
@@ -0,0 +1,6 @@
+won_bet = False
+
+big_win = False
+
+if won_bet or big_win:
+	print("You can now stop betting!")
--- Lesson Folder/If_statement.py
+++ Lesson Folder/If_statement.py
@@ -5,3 +5,5 @@ big_win = True
 if won_bet and big_win :
 
 	print("You can now stop betting!")
+
+	
