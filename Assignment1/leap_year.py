e5f514ff2de46c3592fa24408328aad8b57385d8
zatostephen <88582953+zatostephen@users.noreply.github.com>
2021-09-17 20:20:52 -0800

    leap year assignment

--- Assignment1/leap_year.py
+++ Assignment1/leap_year.py
@@ -1,26 +1,23 @@
-
-#A leap year containing an extra day. It has 366 days instead of the normal 365 days.
-#The extra day is added in February, which has 29 days instead of the normal 28 days.
-#Leap years occur every 4 years. 2020 is a leap year and so is 2024.
-#Except that every 100 years special rules apply. For example 1900 was not a leap year, but 2000 was.
-
 while True:
-    
-    year=input("Enter Year:")
-
+    year= input("Enter Year: ")
     if year.capitalize()=="Exit":
         break
-    elif year.capitalize!="exit" and year.isnumeric()==False:
-        print("Invalid Entry,kindly enter a valid year")
+    elif year.capitalize()!="exit" and  year.isnumeric()==False:
+        print("Invalid Entry.Year entered is either less than zero or is not numeric,Kindly enter a new year or type exit to quit")
+    
+    
     else:
         year=int(year)
-        if year%4!=0:
-           print(year,"is a normal year")
+        if year<0:
+         print("The year",year,"is not a valid year") 
+        elif year%4!=0:
+            print("The Year",year,"is a normal year")
+            
         elif year%100==0:
             if year%400==0:
-                print(year,"is a leap year")
+                print("The Year",year,"is a leap year")
             else:
-                print(year,"Normal year")
-        
+                print("The Year",year,"is a normal year")
         else:
-            print(year,"is a leap year")
+           print("The Year",year,"is a leap year")
+    
